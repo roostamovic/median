e7272301195fa50aeaae014e3666ded9aa5a34aa
Doston Roostamovic <roostamovic@yahoo.com>
2022-02-02 16:06:03 +0500

    Update median.py

--- median.py
+++ median.py
@@ -1,30 +1,25 @@
 # MEDIAN and INTERQUARTILE RANGE
-nums = input('Enter list of numbers: ')
-nums = nums.split()
-num = []
-for i in nums:
-    i = int(i)
-    num.append(i)
-num = sorted(num)
-if len(num) % 2 == 1:
-    median = num[(len(num)+1)//2 - 1]
-    if (len(num)-1) % 4 == 0:
-        Q1 = num[(len(num)-1)//4]
-        Q3 = num[(len(num)-1)//4*3]
+def calculate_median_iqr(nums):
+    nums = sorted(list(nums))
+    if len(nums) % 2 == 1:
+        median = nums[(len(nums)+1)//2 - 1]
+        if (len(nums)-1) % 4 == 0:
+            Q1 = nums[(len(nums)-1)//4]
+            Q3 = nums[(len(nums)-1)//4*3]
+        else:
+            Q1 = (nums[(len(nums)-1)//4] + nums[(len(nums)-1)//4 + 1])/2
+            Q3 = (nums[(len(nums)-1)//4+len(nums)//2]+nums[(len(nums)-1)//4+1 +len(nums)//2])/2
+        IQR = Q3 - Q1
+        
     else:
-        Q1 = (num[(len(num)-1)//4] + num[(len(num)-1)//4 + 1])/2
-        Q3 = (num[(len(num)-1)//4+len(num)//2]+num[(len(num)-1)//4+1 +len(num)//2])/2
-    IQR = Q3 - Q1
-    print(f'Median is {median}')
-    print(f'IQR is {IQR}')
-else:
-    median = (num[len(num)//2 - 1] + num[len(num)//2])/2
-    if len(num) % 4 == 0:
-        Q1 = (num[len(num)//4-1] + num[len(num)//4])/2
-        Q3 = (num[len(num)//4-1 + len(num)//2] + num[len(num)//4 + len(num)//2])/2
-    else:
-        Q1 = num[len(num)//4]
-        Q3 = num[len(num)//4 + len(num)//2]
-    IQR = Q3 - Q1
-    print(f'Median is {median}')
-    print(f'IQR is {IQR}')
+        median = (nums[len(nums)//2 - 1] + nums[len(nums)//2])/2
+        if len(nums) % 4 == 0:
+            Q1 = (nums[len(nums)//4-1] + nums[len(nums)//4])/2
+            Q3 = (nums[len(nums)//4-1 + len(nums)//2] + nums[len(nums)//4 + len(nums)//2])/2
+        else:
+            Q1 = nums[len(nums)//4]
+            Q3 = nums[len(nums)//4 + len(nums)//2]
+        IQR = Q3 - Q1
+        
+    print(f'Median of {nums} is {median}')
+    print(f'IQR of {nums} is {IQR}')
